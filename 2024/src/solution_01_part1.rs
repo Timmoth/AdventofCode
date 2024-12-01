f9f587ebe803e74037de628d4a74e9230b954a97
timmoth <t.jones@timmoth.com>
2024-12-01 09:25:22 +0000

    Use include_str macro to read input file at compile time

--- 2024/src/solution_01_part1.rs
+++ 2024/src/solution_01_part1.rs
@@ -1,14 +1,11 @@
-use std::fs::read_to_string;
-
 pub fn run(){
     println!("Executing day 1 part 1");
-    let file_path = "./inputs/01_actual.txt";
 
     let mut list_a: Vec<i32> = Vec::new();
     let mut list_b: Vec<i32> = Vec::new();
 
     let mut line_count = 0;
-    for line in read_to_string(file_path).unwrap().lines(){
+    for line in include_str!("../inputs/01_actual.txt").lines(){
         let mut parts = line.split_whitespace();
         let a: i32 = parts.next().unwrap().parse().unwrap();
         let b: i32 = parts.next().unwrap().parse().unwrap();
@@ -16,7 +13,7 @@ pub fn run(){
         list_b.push(b);
         line_count += 1;
     }
-    
+
     list_a.sort_unstable();
     list_b.sort_unstable();
 
--- 2024/src/solution_01_part2.rs
+++ 2024/src/solution_01_part2.rs
@@ -1,14 +1,11 @@
-use std::fs::read_to_string;
-
 pub fn run(){
     println!("Executing day 1 part 2");
-    let file_path = "./inputs/01_actual.txt";
 
     let mut list_a: Vec<i32> = Vec::new();
     let mut counts: [i32; 100000] = [0; 100_000];
 
     let mut line_count = 0;
-    for line in read_to_string(file_path).unwrap().lines(){
+    for line in include_str!("../inputs/01_actual.txt").lines(){
         let mut parts = line.split_whitespace();
         let a: i32 = parts.next().unwrap().parse().unwrap();
         let b: i32 = parts.next().unwrap().parse().unwrap();
